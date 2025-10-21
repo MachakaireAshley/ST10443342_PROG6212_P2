ad70e4efa4c452e70c908284939b3936ed5238bb
MachakaireAshley <ST10443342@imconnect.edu.za>
2025-10-21 13:45:32 +0200

    Third Commit
    This Commit shows my ApplicationDbContext and all the seed Data needed

--- CMCS/CMCS/obj/Debug/net9.0/CMCS.AssemblyInfo.cs
+++ CMCS/CMCS/obj/Debug/net9.0/CMCS.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b3b43e34572c80b3906507cb5e5e7afb5e0ffd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e852dcec12deca0fc45b83d5186e37ea6bb0ea2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

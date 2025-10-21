201bf9ebd89be99f40d9e5c74e8f9982406895fb
MachakaireAshley <ST10443342@imconnect.edu.za>
2025-10-21 14:36:35 +0200

    Eighth Commit
    this Commit shows my fully running MVC application

--- CMCS/CMCS/obj/Debug/net9.0/CMCS.AssemblyInfo.cs
+++ CMCS/CMCS/obj/Debug/net9.0/CMCS.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0d0c2470d058efe52de7b9d02938b32062fcd54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bb5ae7b35c903bd00ec35cbb9009d275280971b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
